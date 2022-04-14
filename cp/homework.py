1f20ecd193cc91f659f99b64187d860889cfc2f9
Hung-Yu <jaes1237712@gmail.com>
2022-04-14 18:12:19 +0800

    cp

--- cp/homework.py
+++ cp/homework.py
@@ -1,17 +1,18 @@
-import math
+import math as mp
 import scipy.integrate as integrate
 #prob,err = integrate.quad(func,-n,n,epsabs=1E-10,epsrel=1E-10, args=(1))
 
 def func(x,E,M,Gamma,sigma):
-    return math.exp((-1*(math.pow(x,2)))/(2*(math.pow(sigma,2))))/(math.pow((math.pow(E-x,2)-math.pow(M,2)),2)+M*M*Gamma*Gamma)
+    temp = E-x
+    return mp.exp((-1*(mp.pow(x,2)))/(2*(mp.pow(sigma,2))))/(mp.pow((mp.pow(temp,2)-mp.pow(M,2)),2)+mp.pow(M*Gamma,2))
 
 def convoluted_BreitWigner(E, M, Gamma, sigma):
     value = 0.
     ### START YOUR CODE HERE ###
-    value = integrate.quad(func,-math.inf,math.inf,epsabs=1E-10,epsrel=1E-10,args=(E,M,Gamma,sigma))[0]
+    value = integrate.quad(func, -3*sigma, 3*sigma, epsabs=1E-10,epsrel=1E-10,args=(E,M,Gamma,sigma))[0]
     #### END YOUR CODE HERE ####
     return value
 
-#Test point E=-3.2438, M=+1.2559, Gamma=+1.2073, sigma=+0.4605
-#F(E, M, Gamma, sigma) expected=+0.01890289 result=+0.01927053 --- Failed
-print(convoluted_BreitWigner(-3.2438, 1.2559, 1.2073, 0.4605))
+# Test point E=+2.8562, M=+2.3097, Gamma=+0.3054, sigma=+0.4376
+print(convoluted_BreitWigner(+2.8562,+2.3097,+0.3054,+0.4376))
+
