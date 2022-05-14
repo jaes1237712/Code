5b77a8464f633d9751d6d6cb6e3805dd68ad6bc4
Hung-Yu <jaes1237712@gmail.com>
2022-05-15 07:12:57 +0800

    reset

--- cp/test.py
+++ cp/test.py
@@ -1,30 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+# 0.00340883937	-0.00064
+# 0.005079170661	-0.00094
+# 0.006851767133	-0.00126
+# 0.008522098424	-0.00157
+B_list = np.array([[0.00340883937,0.005079170661,0.006851767133,0.008522098424]])
+V_H_list = np.array([[-0.00064*(0.9),-0.00094*1.1,-0.00126,-0.00157*1.3]])
+# plt.scatter(B_list,V_H_list)
+# for i in range(len(B_list)):
+#     plt.plot([B_list[i],0], [V_H_list[i],0])
+regressor = LinearRegression()
 
-N, L, V0, w = 80, 1., 10., 1.8
-U = np.zeros((N+1,N+1))
-U[N*1//4,N//4:N*3//4+1] = -V0
-U[N*3//4,N//4:N*3//4+1] = +V0
-for step in range(1000):
-    max_delta = 0.
-    for i in range(1,N):
-        for j in range(1,N):
-            if i==N*1//4 and j>=N//4 and j<=N*3//4: continue
-            if i==N*3//4 and j>=N//4 and j<=N*3//4: continue
-            delta = (U[i+1,j]+U[i-1,j]+U[i,j+1]+U[i,j-1])/4. - U[i,j]
-            U[i,j] += w*delta
-            if abs(delta)>max_delta: max_delta = abs(delta)
-    print('Step: %3d, max_delta = %g' % (step,max_delta))
-    if max_delta<1E-4: break
-    
-Ex, Ey = np.zeros((N+1,N+1)), np.zeros((N+1,N+1))
-for i in range(1,N):
-    for j in range(1,N):
-        Ex[i,j] = -(U[i,j+1]-U[i,j-1])/(2.*L/N)
-        Ey[i,j] = -(U[i+1,j]-U[i-1,j])/(2.*L/N)
-    
-fig = plt.figure(figsize=(6,6), dpi=80)
-x = y = np.linspace(0.,L,N+1)
-vx, vy = np.meshgrid(x,y)
-plt.quiver(vx,vy,Ex,Ey)
-plt.show()
+regressor.fit(B_list,V_H_list)
+plt.plot(B_list[0],regressor.predict(B_list)[0], color='k')
+print(regressor.predict(B_list)[0])
--- test.py
+++ test.py
@@ -1,1 +1,2 @@
-print("123")
+for i in range(123):
+    print("1231ol2ik3jmkklqasmkald;msdfkl;asmdklfggamsdkl;fasd;fvlamnsdklfmaskdlfmaksldfmaksldfmkaslwdmfklmaksldfmklasmdkfl")
