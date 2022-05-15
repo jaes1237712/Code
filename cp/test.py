e1023beca843ea620114e520000cd11ce9fb35ab
Hung-Yu <jaes1237712@gmail.com>
2022-05-15 08:22:29 +0800

    cp

--- cp/test.py
+++ cp/test.py
@@ -1,17 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
-# 0.00340883937	-0.00064
-# 0.005079170661	-0.00094
-# 0.006851767133	-0.00126
-# 0.008522098424	-0.00157
-B_list = np.array([[0.00340883937,0.005079170661,0.006851767133,0.008522098424]])
-V_H_list = np.array([[-0.00064*(0.9),-0.00094*1.1,-0.00126,-0.00157*1.3]])
-# plt.scatter(B_list,V_H_list)
-# for i in range(len(B_list)):
-#     plt.plot([B_list[i],0], [V_H_list[i],0])
-regressor = LinearRegression()
+from sklearn import svm
+from sklearn.model_selection import GridSearchCV
 
-regressor.fit(B_list,V_H_list)
-plt.plot(B_list[0],regressor.predict(B_list)[0], color='k')
-print(regressor.predict(B_list)[0])
+for i in range(3):
+    seq = [i, (i+1)%3, (i+2)%3] # A,B,C B,C,A C,A,B
+    print(seq)
